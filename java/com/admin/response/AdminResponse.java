6b72b4e4006752a40125d3cc1a431ce605d072b7
Qineosoft <cbsteam@qineosoftware.com>
2025-11-14 14:46:42 +0530

    initial commit

---
+++ java/com/admin/response/AdminResponse.java
@@ -0,0 +1,22 @@
+package com.admin.response;
+
+import java.util.Date;
+
+public class AdminResponse {
+
+	private Long id;
+
+	private String adminId;
+
+	private String email;
+
+	private String password;
+
+	private String status;
+
+	private Date createdAt;
+
+	private Date updatedAt;
+
+	private String otp;
+}
